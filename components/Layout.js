9871bf64cfdd63c6910b8fb24d4d975170b6e513
Sergej Ernst <sergej.ernst@outlook.com>
2021-03-08 16:21:35 +0100

    Change page title

--- components/Layout.js
+++ components/Layout.js
@@ -6,13 +6,11 @@ export default function Layout({ children }) {
   return (
     <div className='transition-all duration-700 bg-primary dark:bg-primary-dark font-body'>
       <Head>
-        <title>Create Next App</title>
+        <title>Temperator</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      {/* <div className='flex flex-col items-center justify-center font-light dark:text-primary text-primary-dark'> */}
       {children}
-      {/* </div> */}
     </div>
   );
 }
